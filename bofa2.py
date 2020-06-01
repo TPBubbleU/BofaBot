46b6c2c64cb435f74442fcaf722d1b328e686945
Nick Lester <nlester@sunrun.global>
2020-05-31 21:48:20 -0700

    file imports and old passwords in file

--- bofa2.py
+++ bofa2.py
@@ -1,8 +1,8 @@
 import Config
 import discord, time, re, asyncio
 from discord.ext import commands
-import youtube_dl
-import mysql.connector
+import youtube_dl, mysql.connector
+from bofa import get_mysql_db, close_mysql_db
 
 bot = commands.Bot(command_prefix='$')
 
@@ -51,6 +51,7 @@ class YTDLSource(discord.PCMVolumeTransformer):
 async def on_ready():
     print('Bot 2 Logged on as', bot.user)
 
+
 @bot.event
 async def on_message(message):
     # don't respond to ourselves
@@ -58,24 +59,19 @@ async def on_message(message):
         return
     p = re.compile('bofa walkon', re.IGNORECASE)
     if p.match(message.content):
-        p1 = re.compile('url=([^\s]*)')
-        m1 = p1.search(message.content)
-        p2 = re.compile('playtime=([^\s]*)')
-        m2 = p2.search(message.content)
+        m1 = re.compile('url=([^\s]*)').search(message.content)
+        m2 = re.compile('playtime=([^\s]*)').search(message.content)
         if m1 and m2:
-            mydb = mysql.connector.connect(host="localhost",user="root",passwd="w")
-            cursor = mydb.cursor()
-            cursor.execute("USE discord;")
+            cursor, mydb = get_mysql_db()
             cursor.execute("DELETE FROM walkon WHERE username = \""+str(message.author)+"\";")
             sql_insert_query = "INSERT INTO walkon VALUES(\""+str(message.author)+"\",\""+m1.group(1)+"\","+m2.group(1)+");"
             print(sql_insert_query)
             cursor.execute(sql_insert_query)
-            mydb.commit()
-            cursor.close()
-            mydb.close()
+            close_mysql_db(mydb=mydb, cursor=cursor, commit=False)
             await message.channel.send('I got you')
     elif 'bofa' in message.content:
         await message.channel.send('Whats bofa?')
+
 		
 @bot.event
 async def on_voice_state_update(member, before, after):
